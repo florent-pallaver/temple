85a454dbab6550f36984ff9594312a8101b0fa7e
florent-pallaver <florent.pallaver@orange.fr>
2013-12-06 14:01:43 +1100

    0.1
    Signed-off-by: florent-pallaver <florent.pallaver@orange.fr>

---
+++ temple.cdi.client/src/com/temple/cdi/util/ObjectWrapper.java
@@ -0,0 +1,36 @@
+package com.temple.cdi.util;
+
+import java.io.Serializable;
+
+/**
+ * TODOC
+ * 
+ * @author Florent Pallaver
+ * @version 1.0
+ */
+public class ObjectWrapper<X> implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	private X wrappedObject;
+
+	ObjectWrapper() {}
+
+	/**
+	 * Constructor.
+	 * TODOC
+	 * 
+	 * @param object
+	 */
+	public ObjectWrapper(X object) {
+		super();
+		this.wrappedObject = object;
+	}
+
+	/**
+	 * @return the wrappedObject
+	 */
+	public X getWrappedObject() {
+		return this.wrappedObject;
+	}
+}
