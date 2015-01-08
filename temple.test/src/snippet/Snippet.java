efbc1824e93d389e952c0df8db5cdff31934f7e7
flominou <flominou@Leo>
2015-01-08 11:10:03 +0100

    Signed-off-by: flominou <flominou@Leo>

---
+++ temple.test/src/snippet/Snippet.java
@@ -0,0 +1,20 @@
+package snippet;
+
+import java.io.File;
+
+/**
+ * TODOC
+ *
+ * @author Florent Pallaver
+ * @version 1.0
+ */
+public class Snippet {
+
+	public static void main(String[] args) {
+		final File dir = new File("/tmp/thumb/");
+		dir.mkdir();
+		// VideoProcessor.getInstance().createPreviews(
+		// new File("/media/flominou/596d3095-a2c1-4a81-95a6-e0928c63efd6/public/dwhelper/480_800_fMsuD-G179-.mp4"), 20,
+		// 200, ImageType.PNG, "fuck", dir);
+	}
+}
